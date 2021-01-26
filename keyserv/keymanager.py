23c3533d85eda3b41c8cab3af00f05b14af501e6
Oleg Yariga <oyariga@webiomed.ai>
2021-01-26 22:59:53 +0300

    Add key validation mapping sections

--- keyserv/keymanager.py
+++ keyserv/keymanager.py
@@ -117,3 +117,113 @@ def cut_key_unsafe(activations: int, app_id: int,
     return token
 
 
+def disable_key_unsafe(token: str):
+    """Disable a key by its token."""
+    key = Key.query.filter(Key.token == token).first()
+    if not key:
+        current_app.logger.error(
+            f"failed to disable key by non-existent token {token}")
+        raise KeyNotFound(f"no key found for token {token}")
+    key.enabled = False
+    current_app.logger.info(f"disabled key {key}")
+    AuditLog.from_key(key, "key was disabled", Event.KeyModified)
+    db.session.commit()
+
+
+def _compare(left: str, right: str) -> int:
+    if len(left) != len(right):
+        return 0
+    res = 0
+    for leftchr, rightchr in zip(left, right):
+        res |= ord(leftchr) ^ ord(rightchr)
+    return res % 1
+
+
+def key_exists_const(app_id: int, token: str, origin: Origin) -> bool:
+    """Constant time check to see if `token` exists in the database. Compares
+    against all keys even if a match is found."""
+    current_app.logger.info(f"key lookup by token {token}")
+    found = False
+    for key in Key.query.all():
+        if (compare_digest(token, key.token) and
+                key.enabled and key.app_id == app_id):
+
+            found = True
+            key.last_check_ts = datetime.utcnow()
+            key.last_check_ip = origin.ip
+            key.total_checks += 1
+            AuditLog.from_key(key, f"key check from {origin}", Event.KeyAccess)
+    return found
+
+
+def key_valid_const(app_id: int, token: str, origin: Origin) -> bool:
+    """Constant time check to see if `token` exists in the database. Compares
+    against all keys even if a match is found. Validates against the app id
+    and the hardware id provided."""
+    current_app.logger.info(f"key lookup by token {token} from {origin}")
+    found = False
+    for key in Key.query.all():
+        if (compare_digest(token, key.token) and
+                key.enabled and key.app_id == app_id
+                and compare_digest(origin.hwid, key.hwid)):
+
+            found = True
+            key.last_check_ts = datetime.utcnow()
+            key.last_check_ip = origin.ip
+            key.total_checks += 1
+            AuditLog.from_key(key, f"key check from {origin}", Event.KeyAccess)
+    return found
+
+def key_get_unsafe(app_id: int, token: str, origin) -> Key:
+    """Get a key by its token using constant time comparison."""
+
+    current_app.logger.info(f"key retreival by token {token} from {origin}")
+
+    key = Key.query.filter_by(app_id=app_id, token=token, enabled=True).first()
+    if key:
+        AuditLog.from_key(key, f"key retreival from {origin}", Event.KeyAccess)
+        return key
+    return None
+
+
+def activate_key_unsafe(app_id: int, token: str, origin: Origin):
+    """Mark a key as activated by its token. Does not perform constant time
+    comparisons.
+
+    `ip`, `machine`, and `user` are of the originating activation attempt.
+    """
+    key = Key.query.filter_by(token=token, app_id=app_id, enabled=True).first()
+
+    if key.remaining == -1:
+        key.hwid = origin.hwid
+        current_app.logger.info(
+            f"new unlimited activation: Key {key!r} from {origin}")
+        AuditLog.from_key(
+            key, f"new unlimited activation from from {origin}",
+            Event.AppActivation)
+        return
+
+    if key.remaining == 0:
+        current_app.logger.info(
+            f"failed activation attempt: Key {key!r} from {origin}")
+        AuditLog.from_key(
+            key, f"failed activation attempt from {origin}",
+            Event.FailedActivation)
+
+        raise ExhuastedActivations(
+            f"token {token} has exhausted all remaining activations")
+
+    key.remaining -= 1
+
+    current_app.logger.info(f"new activation: Key {key!r} from {origin}."
+                            f" remaining activations: {key.remaining}")
+
+    key.total_activations += 1
+    key.last_activation_ts = datetime.utcnow()
+    key.last_activation_ip = origin.ip
+    key.hwid = origin.hwid
+
+    AuditLog.from_key(
+        key, f"new activation from {origin}", Event.AppActivation)
+
+    db.session.commit()
